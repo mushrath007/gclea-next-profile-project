e2f1cee29d6df19d33a6224101cbbce429a37dbc
Mushrath Mohammad <Mushrath.Mohammad@td.com>
2018-03-10 18:01:17 -0500

    add members

--- src/app/model/team-metadate.model.ts
+++ src/app/model/team-metadate.model.ts
@@ -1,16 +1,16 @@
 export const TeamInfoMetadata = [
   {
     id: 1,
-    name: 'TD Bank',
-    currentOccupation: 'Angular1.x, Javascript, Bootstrap',
+    name: 'Mohammad Qamar',
+    currentOccupation: 'Aim :Data Analysis, Background:Javascript',
     iconPath: '/assets/images/placeholder_male.png',
     githubLinks: 'https://esecure.tdbank.com/net/oao/index.html#/?productname=td%20simple%20checking',
     GitHref: '',
     gitlinkShow: false
   },{
     id: 2,
-    name: 'TD Bank',
-    currentOccupation: 'Angular1.x, Javascript, Bootstrap',
+    name: 'Imran Duka',
+    currentOccupation: 'Background: HTML,CSS,Javascript',
     iconPath: '/assets/images/placeholder_male.png',
     githubLinks: 'https://esecure.tdbank.com/net/oao/index.html#/?productname=td%20simple%20checking',
     GitHref: '',
@@ -18,8 +18,8 @@ export const TeamInfoMetadata = [
   },
   {
     id: 3,
-    name: 'TD Bank',
-    currentOccupation: 'Angular1.x, Javascript, Bootstrap',
+    name: 'Abdur Rahman',
+    currentOccupation: 'Background: HTML,CSS,Javascript',
     iconPath: '/assets/images/placeholder_male.png',
     githubLinks: 'https://esecure.tdbank.com/net/oao/index.html#/?productname=td%20simple%20checking',
     GitHref: '',
@@ -27,8 +27,8 @@ export const TeamInfoMetadata = [
   },
   {
     id: 4,
-    name: 'TD Bank',
-    currentOccupation: 'Angular1.x, Javascript, Bootstrap',
+    name: 'Zakariya wyatt',
+    currentOccupation: 'Background: HTML,CSS,Javascript',
     iconPath: '/assets/images/placeholder_male.png',
     githubLinks: 'https://esecure.tdbank.com/net/oao/index.html#/?productname=td%20simple%20checking',
     GitHref: '',
@@ -36,8 +36,8 @@ export const TeamInfoMetadata = [
   },
   {
     id: 5,
-    name: 'TD Bank',
-    currentOccupation: 'Angular1.x, Javascript, Bootstrap',
+    name: 'Faizi',
+    currentOccupation: 'Background: HTML,CSS,Javascript',
     iconPath: '/assets/images/placeholder_male.png',
     githubLinks: 'https://esecure.tdbank.com/net/oao/index.html#/?productname=td%20simple%20checking',
     GitHref: '',
